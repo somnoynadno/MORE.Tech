529acadeaa694e9567dc3dc34bbf60411ef129dd
Alexander Zorkin <somnoynadno@yandex.ru>
2021-10-09 11:42:55 +0700

    Ping DB before startup

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "MORE.Tech/backend/server"
+import (
+	"MORE.Tech/backend/db"
+	"MORE.Tech/backend/server"
+	"log"
+)
 
 func main() {
+	err := db.GetDB().DB().Ping()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
 	server.RunForever()
 }
